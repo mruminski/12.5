85da7f9d26e312abcf726e3462d6d7f624cb7b1c
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-09-07 19:27:20 +0200

    Show quote on page loaded

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var url = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
+document.addEventListener('DOMContentLoaded', function() {
+  getData();
+});
+
+
+
+var prepareTweet = function(input) {
+  var data = input[0];
+  var element = document.createElement('div');
+  element.innerHTML = data.content;
+  var quote = element.innerText.trim();
+  var author = data.title;
+
+  if (!author.length) { return 'Unknown author' };
+  var toTweet = 'Quote ' + quote + ' by ' + author;
+
+  if (toTweet.length > 280) { 
+    getData();
+  } else {
+    var msg = tweetLink + encodeURIComponent(toTweet);
+    document.querySelector('.container__quote').innerText = quote;
+    document.querySelector('.container__author').innerText = 'by: ' + author;
+    document.querySelector('.container__tweet').setAttribute('href', msg);
+  }
+}
+
 var getData = function() {
   fetch(url, { cache: 'no-store'})
   .then(function(response) {
